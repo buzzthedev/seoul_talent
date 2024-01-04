fe29a656a7586fd8e4481d201e64c2d4dd6ceab3
Buzz <69213001+buzzthedev@users.noreply.github.com>
2024-01-04 23:37:28 +0000

    Initial commit

--- components/FeaturesGrid/FeaturesGrid.tsx
+++ components/FeaturesGrid/FeaturesGrid.tsx
@@ -1,8 +1,8 @@
-import {Container, rem, SimpleGrid, Text, ThemeIcon, Title} from '@mantine/core';
-import {IconCookie, IconGauge, IconLock, IconMessage2, IconUser} from '@tabler/icons-react';
+import { Container, rem, SimpleGrid, Text, ThemeIcon, Title } from '@mantine/core';
+import { IconCookie, IconGauge, IconLock, IconMessage2, IconUser } from '@tabler/icons-react';
 import classes from './FeaturesGrid.module.css';
 
-export const MOCKDATA = [
+const MOCKDATA = [
     {
         icon: IconGauge,
         title: 'Elevate Your Events',
@@ -41,31 +41,31 @@ export const MOCKDATA = [
     },
 ];
 
-export function Feature({icon: Icon, title, description}) {
+function Feature({ icon: Icon, title, description }) {
     return (
         <div>
             <ThemeIcon variant="light" size={40} radius={40}>
-                <Icon style={{width: rem(18), height: rem(18)}} stroke={3}/>
+                <Icon style={{ width: rem(18), height: rem(18) }} stroke={3} />
             </ThemeIcon>
             <Text size="lg" mt="sm" mb={7}>
                 {title}
             </Text>
-            <Text c="grey" lh={1.6}>
+            <Text color="grey" lineHeight={1.6}>
                 {description}
             </Text>
         </div>
     );
 }
 
-export function FeaturesGrid() {
-    const features = MOCKDATA.map((feature, index) => <Feature {...feature} key={index}/>);
+function FeaturesGrid() {
+    const features = MOCKDATA.map((feature, index) => <Feature {...feature} key={index} />);
 
     return (
         <Container className={classes.wrapper}>
             <Title className={classes.title}>The best DJs in United Kingdom</Title>
 
             <Container size={700} p={0}>
-                <Text c="grey" className={classes.description}>
+                <Text color="grey" className={classes.description}>
                     Seoul Talent is your premier online platform offering a curated selection of top-tier DJs, tailor-made for
                     creating unforgettable experiences at private parties and venues. Elevating every occasion, our website
                     simplifies the process of hiring skilled DJs who bring the perfect rhythm and vibe to your event
@@ -74,12 +74,14 @@ export function FeaturesGrid() {
 
             <SimpleGrid
                 mt={60}
-                cols={{base: 1, sm: 2, md: 3}}
-                spacing={{base: 'xl', md: 50}}
-                verticalSpacing={{base: 'xl', md: 50}}
+                cols={{ base: 1, sm: 2, md: 3 }}
+                spacing={{ base: 'xl', md: 50 }}
+                verticalSpacing={{ base: 'xl', md: 50 }}
             >
                 {features}
             </SimpleGrid>
         </Container>
     );
 }
+
+export { FeaturesGrid, MOCKDATA };
