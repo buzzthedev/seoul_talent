38284f05ad37bcbb388cba0e9f3e55c124531b9f
Buzz <69213001+buzzthedev@users.noreply.github.com>
2024-01-06 17:51:25 +0000

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,57 +6,70 @@ import Contact from "../components/Contact";
 
 export default function Page() {
     return (
-        <Container size={1200}>
-            <Flex my="1rem" justify="space-between">
-                <Image className="logo" src="/logo_white.png" alt="Logo" width="180" height="66"/>
-                <Text size="sm">Contact us</Text>
-            </Flex>
-            <Container>
-                <Container className="background-video">
-                    <video playsInline loop muted autoPlay
-                           src="/video.mov"></video>
+        <>
+            <Container size={1200}>
+                <Flex my="1rem" justify="space-between">
+                    <Image className="logo" src="/logo_white.png" alt="Logo" width="180" height="66"/>
+                    <Text size="sm">Contact us</Text>
+                </Flex>
+                <Container>
+                    <Container className="background-video">
+                        <video playsInline loop muted autoPlay
+                               src="/video.mov"></video>
+                    </Container>
+                    <Stack gap="0.2rem" my="8rem">
+                        <Text ta="center" c="indigo">Welcome to Seoul Talent</Text>
+                        <Title mx="auto" ta="center" size="3rem" maw="46rem">Your Ultimate Destination for Unmatched DJ
+                            Experiences</Title>
+                    </Stack>
                 </Container>
-                <Stack gap="0.2rem" my="8rem">
-                    <Text ta="center" c="indigo">Welcome to Seoul Talent</Text>
-                    <Title mx="auto" ta="center" size="3rem" maw="46rem">Your Ultimate Destination for Unmatched DJ Experiences</Title>
-                </Stack>
+
+                <Grid className="djs">
+                    <Grid.Col span={{base: 12, sm: 6}}>
+                        <Paper p="1rem" bg="none">
+                            <Stack gap={0}>
+                                <Image src="/dj1.jpg" alt="DJ 1" width="500" height="250"/>
+                                <Text c="black" mt="0.8rem" fw="bold">DJ 9</Text>
+                                <Text c="dimmed">A short description about the DJ</Text>
+                                <Divider maw="20rem" my="1rem"/>
+                                <Text c="grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                                    doloremque
+                                    eos in iure, laboriosam modi neque quaerat quas rem temporibus?</Text>
+                            </Stack>
+                        </Paper>
+                    </Grid.Col>
+                    <Grid.Col span={{base: 12, sm: 6}}>
+                        <Paper p="1rem" bg="none">
+                            <Stack gap={0}>
+                                <Image src="/dj2.jpg" alt="DJ 1" width="500" height="250"/>
+                                <Text c="black" mt="0.8rem" fw="bold">DJ Louis</Text>
+                                <Text c="dimmed">A short description about the DJ</Text>
+                                <Divider maw="20rem" my="1rem"/>
+                                <Text c="grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                                    doloremque
+                                    eos in iure, laboriosam modi neque quaerat quas rem temporibus?</Text>
+                            </Stack>
+                        </Paper>
+                    </Grid.Col>
+                </Grid>
+
+                <FeaturesGrid/>
+
+                <Title c="black" className={classes.title}>Get in touch</Title>
+                <Container pb="6rem" size={800}>
+                    <Contact/>
+                </Container>
+
             </Container>
-
-            <Grid className="djs">
-                <Grid.Col span={{base: 12, sm: 6}}>
-                    <Paper p="1rem" bg="none">
-                        <Stack gap={0}>
-                            <Image src="/dj1.jpg" alt="DJ 1" width="500" height="250"/>
-                            <Text c="black" mt="0.8rem" fw="bold">DJ Ralph</Text>
-                            <Text c="dimmed">A short description about the DJ</Text>
-                            <Divider maw="20rem" my="1rem"/>
-                            <Text c="grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque
-                                eos in iure, laboriosam modi neque quaerat quas rem temporibus?</Text>
-                        </Stack>
-                    </Paper>
-                </Grid.Col>
-                <Grid.Col span={{base: 12, sm: 6}}>
-                    <Paper p="1rem" bg="none">
-                        <Stack gap={0}>
-                            <Image src="/dj2.jpg" alt="DJ 1" width="500" height="250"/>
-                            <Text c="black" mt="0.8rem" fw="bold">DJ Louis</Text>
-                            <Text c="dimmed">A short description about the DJ</Text>
-                            <Divider maw="20rem" my="1rem"/>
-                            <Text c="grey">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae doloremque
-                                eos in iure, laboriosam modi neque quaerat quas rem temporibus?</Text>
-                        </Stack>
-                    </Paper>
-                </Grid.Col>
-            </Grid>
-
-            <FeaturesGrid/>
-
-            <Title c="black" className={classes.title}>Get in touch</Title>
-            <Container pb="6rem" size={800}>
-                <Contact />
+            <Container py="4rem" bg="#3C404F" size="100%">
+                <Container size={1200}>
+                    <Stack align="center">
+                        <Image className="logo" src="/logo_white.png" alt="Logo" width="180" height="66"/>
+                        <Text my="0.6rem">booking@seoultalent.co.uk</Text>
+                        <Text size="sm" c="dimmed">Copyright Seoul Talent 2024</Text>
+                    </Stack>
+                </Container>
             </Container>
-
-            <Text pb="4rem" c="grey" ta="center">Copyright Seoul Talent 2024</Text>
-        </Container>
+        </>
     );
 }
